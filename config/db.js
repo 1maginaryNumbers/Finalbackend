e2269d3330eaa99e8bb4a485bad0646644d5ed67
Imagine- <simonsugiarta@gmail.com>
2025-11-18 19:23:25 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,16 +2,17 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI is not set in environment variables. Please set it in your .env file");
+    const mongoUri = process.env.MONGODB_URI || process.env.MONGO_URI;
+    
+    if (!mongoUri) {
+      throw new Error("MONGODB_URI or MONGO_URI is not set in environment variables. Please set it in your .env file or Vercel environment variables");
     }
     
-    const mongoUri = process.env.MONGO_URI;
     await mongoose.connect(mongoUri);
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection error:", err);
-    console.log("Please make sure MongoDB is running and MONGO_URI is set in .env file");
+    console.log("Please make sure MongoDB is running and MONGODB_URI or MONGO_URI is set in environment variables");
     console.log("You can use MongoDB Atlas or install MongoDB locally");
     process.exit(1);
   }
